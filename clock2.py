4dd18adae73cd2346fb02123f9d3ff9921963f2b
Scott Slaughter <32985396+scottslaughter@users.noreply.github.com>
2019-10-20 08:15:34 -0500

    Create clock2.py

---
+++ clock2.py
@@ -0,0 +1,40 @@
+##### Python Clock for Raspberry Pi 3B+
+##### scottslaughter
+##### reqires Python3
+##### exit the clock by clicking or pressing ESC
+##### clock style may be adjusted in the style section
+###################################################################
+# import modules
+from tkinter import *
+#import sys
+
+# importing strftime function to get time
+from time import strftime 
+
+# creating fullscreen tkinter widget (root) window
+root = Tk() 
+root.title('Clock') 
+root.configure(background='black',cursor="none")
+root.attributes('-fullscreen', True)
+
+# function to allow ESC to exit
+def exitClock(event):
+    root.destroy()
+root.bind('<Escape>', exitClock)
+root.bind('<Button-1>', exitClock)
+
+# put time into a label (lbl)
+def time():
+    string = strftime('%I:%M %p') 
+    lbl.config(text = string) 
+    lbl.after(1000, time)
+
+# style
+lbl = Label(root, font = ('DejaVu Sans', 66), 
+            background = 'black', 
+            foreground = 'darkorange') 
+lbl.pack(anchor = 'center', pady=90) 
+
+# execute
+time()
+mainloop() 
